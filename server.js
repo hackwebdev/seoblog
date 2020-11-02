1fe9e68320185962a6ce52c57cff9a4a9be75d91
hackwebdev <hackwebdev@gmail.com>
2020-11-02 15:36:00 +0800

    signup route and controller

--- server.js
+++ server.js
@@ -7,6 +7,8 @@ require('dotenv').config();
 
 // bring routes
 const blogRoutes = require('./routes/blog');
+const authRoutes = require('./routes/auth');
+
 
 // app
 const app = express();
@@ -32,6 +34,7 @@ if (process.env.NODE_ENV === 'development') {
 
 // routes middleware
 app.use('/api', blogRoutes);
+app.use('/api', authRoutes);
 
 // port
 const port = process.env.PORT || 8000;
