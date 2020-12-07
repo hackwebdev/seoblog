f0bfdfbe6d4d93494a7fa993b999d0c5c1927884
hackwebdev <hackwebdev@gmail.com>
2020-12-07 14:04:53 +0800

    error in pushing categories and tags

--- controllers/blog.js
+++ controllers/blog.js
@@ -1,3 +1,113 @@
-exports.time = (req, res) => {
-    res.json({ time: Date().toString() });
+const Blog = require("../models/blog");
+const Category = require("../models/category");
+const Tag = require("../models/tag");
+const formidable = require("formidable");
+const slugify = require("slugify");
+const stripHtml = require("string-strip-html");
+const _ = require("lodash");
+const { errorHandler } = require("../helpers/dbErrorHandler");
+const fs = require("fs");
+
+exports.create = (req, res) => {
+  let form = new formidable.IncomingForm();
+  form.keepExtensions = true;
+  form.parse(req, (err, fields, files) => {
+    if (err) {
+      return res.status(400).json({
+        error: "Image could not upload",
+      });
+    }
+    // console.log(files);
+    const { title, body, categories, tags } = fields;
+
+    if (!title || !title.length) {
+      return res.status(400).json({
+        error: "title is required",
+      });
+    }
+
+    if (!body || body.length < 200) {
+      return res.status(400).json({
+        error: "Content is too short",
+      });
+    }
+
+    if (!categories || categories.length === 0) {
+      return res.status(400).json({
+        error: "At least one category is required",
+      });
+    }
+
+    if (!tags || tags.length === 0) {
+      return res.status(400).json({
+        error: "At least one tag is required",
+      });
+    }
+
+    // console.log(body);
+    let blog = new Blog();
+    blog.title = title;
+    blog.body = body;
+    blog.slug = slugify(title).toLowerCase();
+    blog.mtitle = `${title} | ${process.env.APP_NAME}`;
+    // fix tihs error undefined
+    blog.mdesc = stripHtml(body.substring(0, 160)).result;
+    blog.postedBy = req.auth._id;
+    // console.log(blog.mdesc);
+
+    // categories and tags
+    let arrayOfCategories = categories && categories.split(",");
+    // console.log(arrayOfCategories);
+    let arrayOfTags = tags && tags.split(",");
+
+    if (files.photo) {
+      //   console.log(files);
+      if (files.photo.size > 10000000) {
+        return res.status(400).json({
+          error: "Image should be less then 1mb in size",
+        });
+      }
+      blog.photo.data = fs.readFileSync(files.photo.path);
+      blog.photo.contentType = files.photo.type;
+    }
+
+    blog.save((err, result) => {
+      //   console.log(err);
+      if (err) {
+        return res.status(400).json({
+          error: errorHandler(err),
+        });
+      }
+      console.log(result._id);
+
+      //   res.json(result);
+      Blog.findByIdAndUpdate(
+        result._id,
+        // { $push: { categories: arrayOfCategories } },
+        { new: true }
+      ).exec((err, result) => {
+        console.log(err);
+        if (err) {
+          return res.status(400).json({
+            error: errorHandler(err),
+          });
+        } else {
+          Blog.findByIdAndUpdate(
+            result._id,
+            // { $push: { tags: arrayOfTags } },
+            { new: true }
+          ).exec((err, result) => {
+            if (err) {
+              return res.status(400).json({
+                error: errorHandler(err),
+              });
+            } else {
+              console.log(result);
+              res.json(result);
+            }
+          });
+        }
+      });
+    });
+  });
 };
